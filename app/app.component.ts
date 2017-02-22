251043ae669cd57484f0714fdf5022e053ab4451
Ben Copeland <bscopeland@gmail.com>
2017-02-22 12:52:38 -0500

    Adds base app

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: `
+    <ActionBar title="Zombie Mobile"></ActionBar>
+    <!-- Your UI components go here -->
+  `
+})
+export class AppComponent {
+  // Your TypeScript logic goes here
+}
