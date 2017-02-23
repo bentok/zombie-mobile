b659ce27ffeb4bfaef910eec7adbb73dd0f50cdb
Ben Copeland <bscopeland@gmail.com>
2017-02-23 12:40:27 -0500

    Adds GridLayout for main page

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from "@angular/core";
+import { GridLayout } from "ui/layouts/grid-layout";
 
 @Component({
   selector: "my-app",
   template: `
     <ActionBar title="Zombie Mobile"></ActionBar>
-    <!-- Your UI components go here -->
-  `
+    <GridLayout rows="60, 60, auto" columns="60, 60" class="header-container" colSpan="6" columns="*,*,*,*,*">
+      <Button text="Stats" row="0" col="0" colSpan="2" class="button"></Button>
+      <Button text="Inventory" row="0" col="2" colSpan="2" class="button"></Button>
+    </GridLayout>
+  `,
+  styles: [
+    `
+      .button {
+        background: green;
+      }
+    `
+  ]
 })
-export class AppComponent {
-  // Your TypeScript logic goes here
-}
+export class AppComponent {}
